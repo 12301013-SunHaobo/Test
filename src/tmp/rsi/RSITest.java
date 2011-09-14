886dfdca97fbca4a3bff206835cb76492e6799f5
Rong Xia <rxia@wgen.net>
2011-09-14 18:02:30 -0400

    draft

---
+++ src/tmp/rsi/RSITest.java
@@ -0,0 +1,37 @@
+package tmp.rsi;
+
+import java.io.IOException;
+import java.text.ParseException;
+
+import junit.framework.TestCase;
+
+public class RSITest extends TestCase {
+    private String symbolForTest = "GOOG";
+    private int nineDaysPeriod = 9;
+    private int fourteenDaysPeriod = 14;
+    private int twentyFiveDaysPeriod = 25;
+
+    public void testGetPrices() throws Exception {
+        assertTrue(YahooFeederUtil.getPrices(symbolForTest).size() > 1);
+    }
+
+    public void testRSICalculate() throws Exception {
+        try {
+            RSI rsi = new RSI(nineDaysPeriod, symbolForTest);
+            System.out.println("RSI for a " + rsi.getPeriodLength()
+                    + " days period is: " + rsi.calculate());
+
+            rsi = new RSI(fourteenDaysPeriod, symbolForTest);
+            System.out.println("RSI for a " + rsi.getPeriodLength()
+                    + " days period is: " + rsi.calculate());
+
+            rsi = new RSI(twentyFiveDaysPeriod, symbolForTest);
+            System.out.println("RSI for a " + rsi.getPeriodLength()
+                    + " days period is: " + rsi.calculate());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
