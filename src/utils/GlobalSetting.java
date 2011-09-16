7ec068650b499a7a06fc3b1ad937aea9f534481e
andy200367 <andy2003_67@hotmail.com>
2011-09-16 19:25:11 -0400

    GlobalSetting for office and home pc

--- src/utils/GlobalSetting.java
+++ src/utils/GlobalSetting.java
@@ -2,6 +2,21 @@ package utils;
 
 public class GlobalSetting {
 
-	//public static final String TEST_HOME = "D:/projects/java/workspace/Test";
-    public static final String TEST_HOME = "D:/user/workspaces/pk12/Test";
+	public static final String TEST_HOME = getTestHome();
+
+	private static String getTestHome() {
+		if ("r".equals(System.getProperty("user.name", "not set"))) {
+			return "D:/projects/java/workspace/Test";
+		} else if ("rxia".equals(System.getProperty("user.name"))) {
+			return "D:/user/workspaces/pk12/Test";
+		} else {
+			System.err.println("Not office or home computer. Please check GlobalSetting.java");
+			System.exit(-1);
+			return null;
+		}
+	}
+
+	public static void main(String args[]) {
+		System.out.println(TEST_HOME);
+	}
 }
