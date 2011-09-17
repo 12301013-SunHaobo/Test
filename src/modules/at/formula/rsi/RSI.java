782cdc8ac928b0cfdcb9e5ce972a1d85cfafaf88
andy200367 <andy2003_67@hotmail.com>
2011-09-17 04:18:12 -0400

    asdf

--- src/modules/at/formula/rsi/RSI.java
+++ src/modules/at/formula/rsi/RSI.java
@@ -2,82 +2,118 @@ package modules.at.formula.rsi;
 
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 import java.util.Stack;
 
 import modules.at.model.Bar;
 
-public class RSI {
+public class RRecal {
     private int length;
-    private final Stack<Averages> avgList;
-    private final ArrayList<Bar> rsiQ;
+    private final Queue<GL> rQ = new LinkedList<GL>(); // store recent 13 GL
 
-    public RSI(int length, String symbol) throws ParseException, IOException {
-        super();
+    private double prePrice = -1;
+
+    public RRecal(int length) throws Exception {
         this.length = length;
-        avgList = new Stack<Averages>();
-        rsiQ = YahooFeederUtil.getPrices(symbol);
+    }
+
+    public void addPrice(double price) {
+        GL gl = null;
+        if (prePrice == -1) {
+            gl = new GL(0, 0);
+            rQ.add(gl);
+        } else {
+            double diff = price - prePrice;
+
+            double gain = Math.max(0, diff);
+            double loss = Math.max(0, -diff);
+            gl = new GL(gain, loss);
+            rQ.add(gl);
+        }
+        prePrice = price;
+        if(rQ.size()>length){
+            rQ.remove();
+        }
     }
 
     public double calculate() {
-        double value = 0;
-        int pricesSize = rsiQ.size();
-        int lastPrice = pricesSize - 1;
-        int firstPrice = lastPrice - length + 1;
-
-        double gains = 0;
-        double losses = 0;
-        double avgUp = 0;
-        double avgDown = 0;
-
-        double delta = rsiQ.get(lastPrice).getClose() - rsiQ.get(lastPrice - 1).getClose();
-        gains = Math.max(0, delta);
-        losses = Math.max(0, -delta);
-
-        if (avgList.isEmpty()) {
-            for (int bar = firstPrice + 1; bar <= lastPrice; bar++) {
-                double change = rsiQ.get(bar).getClose() - rsiQ.get(bar - 1).getClose();
-                gains += Math.max(0, change);
-                losses += Math.max(0, -change);
-            }
-            avgUp = gains / length;
-            avgDown = losses / length;
-            avgList.push(new Averages(avgUp, avgDown));
-
-        } else {
-
-            Averages avg = avgList.pop();
-            avgUp = avg.getAvgUp();
-            avgDown = avg.getAvgDown();
-            avgUp = ((avgUp * (length - 1)) + gains) / (length);
-            avgDown = ((avgDown * (length - 1)) + losses) / (length);
-            avgList.add(new Averages(avgUp, avgDown));
+        if(rQ.size()<length){
+            return -1;
         }
-        value = 100 - (100 / (1 + (avgUp / avgDown)));
+        
+        double totalGain = 0;
+        double totalLoss = 0;
 
-        return Math.round(value);
+        for(GL gl : rQ){
+            totalGain+=gl.getGain();
+            totalLoss+=gl.getLoss();
+        }
+        double avgGain = totalGain/length;
+        double avgLoss = totalLoss/length;
+        
+        double rs = avgGain/avgLoss;
+        double rsi = 100 - 100/(1+rs);
+        
+        return rsi;
     }
 
-    private class Averages {
+    /*
+     * 
+     * public double calculate() { double value = 0; int pricesSize =
+     * rsiQ.size(); int lastPrice = pricesSize - 1; int firstPrice = lastPrice -
+     * length + 1;
+     * 
+     * double gains = 0; double losses = 0; double avgUp = 0; double avgDown =
+     * 0;
+     * 
+     * double delta = rsiQ.get(lastPrice).getClose() - rsiQ.get(lastPrice -
+     * 1).getClose(); gains = Math.max(0, delta); losses = Math.max(0, -delta);
+     * 
+     * if (avgList.isEmpty()) { for (int bar = firstPrice + 1; bar <= lastPrice;
+     * bar++) { double change = rsiQ.get(bar).getClose() - rsiQ.get(bar -
+     * 1).getClose(); gains += Math.max(0, change); losses += Math.max(0,
+     * -change); } avgUp = gains / length; avgDown = losses / length;
+     * avgList.push(new Averages(avgUp, avgDown));
+     * 
+     * } else {
+     * 
+     * Averages avg = avgList.pop(); avgUp = avg.getAvgUp(); avgDown =
+     * avg.getAvgDown(); avgUp = ((avgUp * (length - 1)) + gains) / (length);
+     * avgDown = ((avgDown * (length - 1)) + losses) / (length); avgList.add(new
+     * Averages(avgUp, avgDown)); } value = 100 - (100 / (1 + (avgUp /
+     * avgDown)));
+     * 
+     * return Math.round(value); }
+     */
 
-        private final double avgUp;
-        private final double avgDown;
+    // both are positive values
+    private class GL {
+        double gain;
+        double loss;
 
-        public Averages(double up, double down) {
-            this.avgDown = down;
-            this.avgUp = up;
+        public GL(double gain, double loss) {
+            super();
+            this.gain = gain;
+            this.loss = loss;
         }
 
-        public double getAvgUp() {
-            return avgUp;
+        public double getGain() {
+            return gain;
         }
 
-        public double getAvgDown() {
-            return avgDown;
+        public void setGain(double gain) {
+            this.gain = gain;
         }
-    }
 
-    public int getPeriodLength() {
-        return length;
+        public double getLoss() {
+            return loss;
+        }
+
+        public void setLoss(double loss) {
+            this.loss = loss;
+        }
+
     }
 }
