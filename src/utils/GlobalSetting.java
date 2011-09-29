1578072fee7030c8b8927f1c2204fb878950d414
rxia <rxia@wgen.net>
2011-09-29 18:51:24 -0400

    get localhost in GlobalSetting

--- src/utils/GlobalSetting.java
+++ src/utils/GlobalSetting.java
@@ -1,9 +1,26 @@
 package utils;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public class GlobalSetting {
 
+    public static final int MOCK_SERVER_PORT = 8900;
+    public static final String MOCK_SERVER_IP = getMockServerIp();
+    
 	public static final String TEST_HOME = getTestHome();
 
+    
+    private static String getMockServerIp(){
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+        return null;
+    }
+    
 	private static String getTestHome() {
 		if ("r".equals(System.getProperty("user.name", "not set"))) {
 			return "D:/projects/java/workspace/Test";
