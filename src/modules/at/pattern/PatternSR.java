57c5416995096f57b79609b01f6ab8b8536b1dda
Rong Xia <rxia@wgen.net>
2011-11-05 00:11:05 -0400

    a

--- src/modules/at/pattern/PatternSR.java
+++ src/modules/at/pattern/PatternSR.java
@@ -1,5 +1,10 @@
 package modules.at.pattern;
 
+/**
+ * 
+ * support/resistance
+ *
+ */
 public class PatternSR {
 
     
---
+++ test/modules/at/pattern/TestFindAllVertexAreas.java
@@ -0,0 +1,27 @@
+package modules.at.pattern;
+
+import java.util.ArrayDeque;
+
+
+/**
+ * Cons: can only be found after
+ * VertexArea:
+ * 1. at least {m} bars in between
+ * 2. 
+ *
+ */
+public class TestFindAllVertexAreas {
+
+    
+    private static ArrayDeque ad = new ArrayDeque();
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        
+        
+
+    }
+
+}
