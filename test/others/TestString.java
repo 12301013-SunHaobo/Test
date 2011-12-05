c366667153f74842d2a433a3ea6ffe4b06306241
Rong Xia <rxia@wgen.net>
2011-12-04 22:29:41 -0500

    a

--- test/others/TestString.java
+++ test/others/TestString.java
@@ -7,14 +7,20 @@ public class TestString {
      */
     public static void main(String[] args) {
         
-        //String featureSettingStr = "setLearningMapEnabled";
-        String testStr = "setEnabledabcEnabled";
-        
-        String resultStr = testStr.replaceAll("^set|Enabled$", "");
-        
-        System.out.println(resultStr);
-        
+        testRemovePackageStr();
 
     }
 
+    
+    private static void testRemovePackageStr(){
+        String fullClassName = "net.wgen.threetwelve.outcomes.ui.web.service.internal.RPTServiceController2Test";
+        System.out.println(fullClassName.replaceAll(".*?\\.", ""));
+    }
+    
+    private static void test1(){
+        //String featureSettingStr = "setLearningMapEnabled";
+        String testStr = "setEnabledabcEnabled";
+        String resultStr = testStr.replaceAll("^set|Enabled$", "");
+        System.out.println(resultStr);
+    }
 }
