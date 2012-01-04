ece8b8e5944c14a0ccd6bf56b9380e0d645f0322
Rong Xia <rxia@wgen.net>
2012-01-04 18:34:09 -0500

    a

--- test/others/anno/ConfigRangeContainer.java
+++ test/others/anno/ConfigRangeContainer.java
@@ -1,19 +1,116 @@
 package others.anno;
 
-public class ParamContainer {
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
-	
+public class ConfigRangeContainer<T> {
 
-    //trade unit, how many shares in one trade unit
-    public static final int TRADE_UNIT = 1;
-	
-	
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+    private boolean valid = false;
+    private List<String> errors = null;
+    
+    private List<T> configList = new ArrayList<T>();//all config instances
+    
+    public ConfigRangeContainer(Class<T> clazz) {
+        super();
+        this.errors = validateConfig(clazz);
+        if(errors.size()==0){
+            this.valid = true;
+        }
+    }
+
+    private populateConfigs(Field f, List<T> configs){
+        
+    }
+    
+    private List<String> validateConfig(Class<T> testClass){
+        List<String> errorList = new ArrayList<String>();
+        Field[] field = testClass.getFields();
+        for(int i=0;i<field.length;i++){
+            Field f = field[i];
+            String name = f.getName();
+            IntRange intRange = f.getAnnotation(IntRange.class);
+            DoubleRange doubleRange = f.getAnnotation(DoubleRange.class);
+
+            //only one range is allowed
+            if(intRange!=null && doubleRange!=null){
+                errorList.add("["+name+"] has both IntRange and DoubleRange, only one is allowed.");
+                continue;
+            }
+            //range must match data type
+            if (Double.TYPE.equals(f.getType()) && intRange!=null){
+                errorList.add("["+name+"] double type has IntRange.");
+                continue;
+            } 
+            if (Integer.TYPE.equals(f.getType()) && doubleRange!=null){
+                errorList.add("["+name+"] int type has DoubleRange.");
+                continue;
+            }
+            //range interval is reasonable, 
+            if (Integer.TYPE.equals(f.getType())){
+                int start = intRange.start();
+                int end = intRange.end();
+                int intervals = intRange.intervals();
+                if (end<start) {
+                    errorList.add("["+name+"] end<start.");
+                    continue;
+                }
+                if ((end-start)<intervals) {
+                    errorList.add("["+name+"] unreasonable intervals.");
+                    continue;
+                }
+            }
+        }
+        return errorList;
+    }
 
 
-	}
 
+    public boolean isValid() {
+        return valid;
+    }
+
+    public void setValid(boolean valid) {
+        this.valid = valid;
+    }
+
+    public List<String> getErrors() {
+        return errors;
+    }
+
+    public void setErrors(List<String> errors) {
+        this.errors = errors;
+    }  
+    
+    
+    
+    private static class ConfigField {
+        
+    }
+}
+
+
+
+
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface IntRange {
+    int start();
+    int end();
+    int intervals();
+    String description() default "";
+}
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface DoubleRange {
+    double start();
+    double end();
+    int intervals();
+    String description() default "";
 }
